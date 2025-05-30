c61eccd5aabd3448107941e47aea8af348810720
Jonathan Sabo <jonathan@sabo.io>
2025-05-30 08:39:08 -0400

    wip

--- gke-us-east1/gke/firewall.tf
+++ gke-us-east1/gke/firewall.tf
@@ -1,28 +1,3 @@
-resource "google_compute_firewall" "scylla" {
-  name    = "scylla-allow-internal"
-  network = google_compute_network.vpc.id
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "7000",  # Inter-node
-      "7001",  # SSL inter-node
-      "9042",  # CQL
-      "9142",  # SSL CQL
-      "9160",  # (Legacy)
-      "9180",  # Prometheus
-      "9100",  # node_exporter (optional)
-      "10000", # REST API
-      "10001", # Scylla Manager Agent REST API (HTTPS)
-      "19042", # Shard-aware CQL
-      "19142"  # Shard-aware CQL (SSL)
-    ]
-  }
-
-  source_ranges = [google_compute_subnetwork.primary.ip_cidr_range]
-  target_tags   = ["scylla"]
-}
-
 resource "google_compute_firewall" "allow_ssh" {
   name    = "allow-ssh-from-your-ip"
   network = google_compute_network.vpc.id
